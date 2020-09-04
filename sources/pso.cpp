c3165503929c3263cdd0748d69a172fe94296ed7
terpsihora96 <dzedaj996@gmail.com>
2020-09-04 14:45:38 +0200

    Couple of tests done on different shapes. Code refactored.

--- sources/pso.cpp
+++ sources/pso.cpp
@@ -6,16 +6,19 @@
 #include <chrono>
 #include <thread>
 #include "../headers/pso.hpp"
+#include "../headers/textures.hpp"
+#include "../headers/write.hpp"
 
 #define UP (1.0)
 #define LO (0.0)
-#define MAX_ITER (1000)
+#define MAX_ITER (60)
 #define size (0.1)
-#define FUNC(x) (1.0/(1.0 + exp(-x)))
+#define FUNC(x) (1.0 / (1.0 + exp(-x)))
 #define NUM_PARTICLES (55)
 #define NUM_MICROSECS (0)
 
-Particle::Particle() {
+Particle::Particle()
+{
    std::random_device rd;
    std::mt19937 gen(rd());
    std::uniform_real_distribution<> p_runif(LO, UP);
@@ -26,50 +29,62 @@ Particle::Particle() {
    m_fitness = FUNC(m_position);
 }
 
-void Particle::updateVelocity(double velocity) {
+void Particle::updateVelocity(double velocity)
+{
    m_velocity = velocity;
 }
 
-void Particle::updatePosition(void) {
+void Particle::updatePosition(void)
+{
    m_position += m_velocity;
 }
 
-void Particle::updateBest(double best) {
+void Particle::updateBest(double best)
+{
    m_best = best;
 }
 
-double Particle::position(void) {
+double Particle::position(void)
+{
    return m_position;
 }
 
-double Particle::best(void) {
+double Particle::best(void)
+{
    return m_best;
 }
 
-double Particle::fitness(void) {
+double Particle::fitness(void)
+{
    return m_fitness;
 }
 
-double Particle::velocity(void) {
+double Particle::velocity(void)
+{
    return m_velocity;
 }
 
-
-double Particle::acceler_coef(void) {
+double Particle::acceler_coef(void)
+{
    return m_acceler_coef;
 }
 
-double Particle::inertia(void) {
+double Particle::inertia(void)
+{
    return m_inertia;
 }
 
-PSO::~PSO() {
-   for (auto p : m_swarm) {
+PSO::~PSO()
+{
+   for (auto p : m_swarm)
+   {
       delete p;
    }
+   free_textures();
 }
 
-void PSO::create_population(int population_size) {
+void PSO::create_population(int population_size)
+{
    m_population_size = population_size;
    std::random_device rd;
    std::mt19937 gen(rd());
@@ -77,54 +92,90 @@ void PSO::create_population(int population_size) {
    m_global_best = runif(gen);
    m_swarm.resize(0);
 
-   for (int i = 0; i < population_size; ++i) {
+   for (int i = 0; i < population_size; ++i)
+   {
       m_swarm.push_back(new Particle());
-      if (FUNC(m_swarm[i]->position()) > m_swarm[i]->fitness()) {
+      if (FUNC(m_swarm[i]->position()) > m_swarm[i]->fitness())
+      {
          m_global_best = m_swarm[i]->position();
       }
    }
 }
 
-double PSO::start() {
+double PSO::start()
+{
    create_population(NUM_PARTICLES);
    std::random_device rd;
    std::mt19937 gen(rd());
    std::uniform_real_distribution<> runif(LO, UP);
    int iter = 0;
-   
-   while (iter < MAX_ITER) {
-      for (auto particle : m_swarm) {
+
+   // PSO algorithm
+   // In each interation compute particles' fitnesses, their positions, velocities
+   // As well as the best solution for the iteration - m_global_best
+   while (iter < MAX_ITER)
+   {
+      for (auto particle : m_swarm)
+      {
+         // Update velocity
          particle->updateVelocity(
-            particle->velocity()  * particle->inertia() + 
-            (particle->acceler_coef() * runif(gen)) * (particle->best() - particle->position()) +
-            (particle->acceler_coef() * runif(gen)) * (m_global_best -  particle->position())
-         );
+             particle->velocity() * particle->inertia() +
+             (particle->acceler_coef() * runif(gen)) * (particle->best() - particle->position()) +
+             (particle->acceler_coef() * runif(gen)) * (m_global_best - particle->position()));
          particle->updatePosition();
          // Find local best position
-         if (FUNC(-particle->position()) > particle->fitness()) {
+         if (FUNC(-particle->position()) > particle->fitness())
+         {
             particle->m_best = particle->position();
             particle->m_fitness = (1.0 / (1.0 + exp(-(particle->position()))));
          }
          // Find global best position
-         if ((1.0 / (1.0 + exp(-(m_global_best)))) > particle->fitness()) {
+         if ((1.0 / (1.0 + exp(-(m_global_best)))) > particle->fitness())
+         {
             m_global_best = particle->position();
          }
-         // Set color
-         glColor3f(1
-                  , particle->fitness()
-                  , abs(particle->position()));
-         
-         glBegin(GL_TRIANGLE_FAN);
-            glVertex3f(cos(particle->position()) / 2, particle->position() + size / 2, 3);
-            glVertex3f(sin(particle->position()), particle->position() + size / 2, 4);
-            glVertex3f(sin(particle->position()), particle->position() - size / 2, 2);
-            glVertex3f(sin(particle->fitness() - size / 2), particle->best() + size / 2, 0);
-         glEnd();
-         glRotatef(particle->fitness(), particle->position(), particle->position(), particle->position());
-         glColor3f(0,1,0);
+
+         // -----------------------------------
+         // Begin animation for this iteration
+         // Use calculated results to create an interesting animation
+
+         // CUBE
+         glColor3f(0.4, particle->fitness(), abs(particle->position()));
+         glTranslatef(1, particle->fitness() / 10, 1 - abs(particle->position()));
+         glRotatef(particle->fitness(), 1, particle->fitness(), 1 - abs(particle->position()));
          glutSolidCube(particle->fitness());
+
+         // FAN
+         // Change texture randomly in each iteration
+         std::srand(static_cast<unsigned int>(std::time(nullptr)));
+         int texture = (rand() % NUMBER_OF_TEXTURES);
+         // Set randomly chosen texture
+         glBindTexture(GL_TEXTURE_2D, texture_names[texture]);
+         glNormal3f(0, 1 - particle->fitness(), 1);
+         glBegin(GL_TRIANGLE_FAN);
+         glTexCoord2f(0, 0);
+         glVertex3f(cos(particle->position()) / 2, particle->position() + size / 2, 3);
+         glTexCoord2f(0, 128);
+         glVertex3f(sin(particle->m_best), particle->position() + size / 2, 4);
+         glTexCoord2f(128, 128);
+         glVertex3f(sin(particle->position()), particle->position() - size / 2, 2);
+         glTexCoord2f(128, 0);
+         glVertex3f(sin(particle->fitness() - size / 2), particle->best() + size / 2, 0);
+         glEnd();
+
+         // TORUS
+         glColor3f(0, 1, abs(particle->position()));
+         if (particle->fitness() > 0)
+         {
+            glutSolidTorus(particle->position() * 0.3, particle->fitness() * 6, NUMBER_OF_TEXTURES, 13);
+         }
+
+         // Draw 0 and + mainly in the lower part of the scene
+         draw_name(std::to_string(0), particle->fitness() * 1000 * m_global_best, particle->best() * 100);
+         draw_name("+", 1000 * particle->position(), particle->best() * 100);
+
          // To make animation a bit smoother
-         std::this_thread::sleep_for(std::chrono::microseconds(NUM_MICROSECS));
+         // std::this_thread::sleep_for(std::chrono::microseconds(NUM_MICROSECS));
       }
       iter++;
    }
